399c53f350d5757bac2dadb9207749e353dea5d1
Eduard Dovgal <eduard.dovgal@gmail.com>
2019-08-21 14:51:55 +0300

    Introduce pagination, part 1

--- app/controllers/articles.js
+++ app/controllers/articles.js
@@ -1,4 +1,9 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
+
+  queryParams: ['page', 'size'],
+  page: 1,
+  size: 5
+
 });
--- app/routes/articles.js
+++ app/routes/articles.js
@@ -1,8 +1,25 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model() {
+
+  queryParams: {
+    page: {
+      refreshModel: true
+    },
+    size: {
+      refreshModel: true
+    }
+  },
+
+  model(params) {
+
+    return this.store.query('article', { page: {
+        number: params.page,
+        size: params.size
+      }
+    });
+
     // return this.get('store').findAll('article');
-    return this.store.findAll('article');
+    //return this.store.findAll('article');
   }
 });
