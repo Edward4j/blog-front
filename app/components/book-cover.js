14b9514e61a0b101277aa68bb3f6b001a9db1aa0
Eduard Dovgal <eduard.dovgal@gmail.com>
2019-08-21 11:54:42 +0300

    Add ember-route-action-helper, blur background, reload model to fetch latest data from backend

--- app/components/book-cover.js
+++ app/components/book-cover.js
@@ -5,11 +5,16 @@ export default Component.extend({
   actions: {
 
     open() {
-      this.set('isShowingModal', true);
+      // We will make sure we get the latest price when opening the modal by reload()ing the model:
+      this.get('book').reload().then(() => {
+        this.set('isShowingModal', true);
+        this.get('blurBackground')(true);
+      });
     },
 
     close() {
       this.set('isShowingModal', false);
+      this.get('blurBackground')(false);
     }
 
   }
---
+++ app/routes/application.js
@@ -0,0 +1,10 @@
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+
+  actions: {
+    blurBackground(blur) {
+      this.controllerFor('application').set('blur', blur);
+    }
+  }
+});
